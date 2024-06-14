47b345aca8528e0bce36a9a5c0686c286507c59a
Carlo-Adile <adile.carlo.a@gmailcom>
2024-06-14 15:21:58 +0200

    add base side nav

---
+++ resources/views/layouts/partials/side.blade.php
@@ -0,0 +1,27 @@
+<section id="my_sidebar">
+	<div class="container">
+		<div class="row flex-column border-end gap-2 p-2">
+			<div class="col">
+				<ul class="list-unstyled d-flex flex-column gap-2">
+					<li>
+						<h4 class="card p-2 text-center">Your Restaurant</h4>
+					</li>
+					<li>
+						<h5><i class="fa-solid fa-chart-simple"></i> Analytics</h5>
+					</li>
+					<li>
+						<h5><i class="fa-solid fa-utensils"></i> Menu</h5>
+					</li>
+					<li>
+						<h5><i class="fa-solid fa-truck"></i> Delivery</h5>
+					</li>
+					<li>
+						<h5>aggiungi altro...</h5>
+					</li>
+				</ul>
+			</div>
+			<hr>
+
+		</div>
+	</div>
+</section>
