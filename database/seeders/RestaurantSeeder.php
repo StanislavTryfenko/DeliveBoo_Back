9921eca81191df5b34d215f1b1d00b51a0f8a9f5
Carlo-Adile <adile.carlo.a@gmailcom>
2024-06-14 16:23:38 +0200

    add seeders for restaurants and users

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,14 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class RestaurantSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 5; $i++) {
+            $restaurant = new Restaurant();
+            $restaurant->name = $faker->words(4, true);
+            $restaurant->slug = Str::of($restaurant->name)->slug('-');
+            $restaurant->contact_email = $faker->safeEmail();
+            $restaurant->vat = $faker->numerify('##########');
+            $restaurant->address = $faker->address;
+            $restaurant->phone_number = $faker->phoneNumber();
+            $restaurant->user_id = $i + 1;
+            $restaurant->save();
+        }
     }
 }
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $name = ['antonio', 'stanislav', 'emanuel', 'carlo', 'andrea'];
+
+        $email = ['risto1@test.com', 'risto2@test.com', 'risto3@test.com', 'risto4@test.com', 'risto5@test.com'];
+
+        for ($i = 0; $i < 5; $i++) {
+            $newUser = new User();
+            $newUser->name = $name[$i];
+            $newUser->email = $email[$i];
+            $newUser->password = bcrypt('password');
+            $newUser->save();
+        }
+    }
+}
