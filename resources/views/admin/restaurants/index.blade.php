768f2107b378d437f0aeeccb647f4f6c13cb0551
Carlo-Adile <adile.carlo.a@gmailcom>
2024-06-15 19:37:51 +0200

    new clear layout for the main view + various methods addition and refactoring

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,145 +1,57 @@
 @extends('layouts.admin')
 
 @section('content')
-	<div class="container">
-		{{-- <h2 class="fs-4 text-secondary my-4">
-			{{ __('Dashboard') }}
-		</h2> --}}
-		<div class="row justify-content-center">
-			<div class="col">
-				<div class="card">
-					{{-- <div class="card-header">{{ __('User Dashboard') }}</div> --}}
-					<div class="card-body">
-						@if (session('status'))
-							<div class="alert alert-success" role="alert">
-								{{ session('status') }}
-							</div>
-						@endif
-						{{-- add your restaurant here --}}
-						@if ($restaurant === null)
-							Form create restaurant
-							<form action="{{ route('admin.restaurants.store') }}" method="POST" enctype="multipart/form-data">
-								@csrf
-								<div class="my-2">
-									<label for="name" class="form-label">Name</label>
-									<input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
-										value="{{ old('name') }}" placeholder="es.Locanda Team 6">
-									<small id="name" class="form-text text-muted">type here the name of your
-										business</small>
-									@error('name')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
+ <div class="container-fluid">
+  <div class="row justify-content-center">
+   <div class="col-12">
 
-								<div class="my-2">
-									<label for="address" class="form-label">Address</label>
-									<input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address"
-										value="{{ old('address') }}" placeholder="es.Via Italia n* 6">
-									<small id="name" class="form-text text-muted">type here the full address of your
-										business</small>
-									@error('address')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
+    {{-- add your restaurant here --}}
+    <section>
+     @if ($restaurant === null)
+      @include('layouts.partials.restaurant.creation')
+     @endif
+    </section>
 
-								<div class="my-2">
-									<label for="phone_number" class="form-label">Phone number</label>
-									<input type="tel" class="form-control @error('phone_number') is-invalid @enderror" id="phone_number"
-										name="phone_number" value="{{ old('phone_number') }}" placeholder="es.333 456 7810">
-									<small id="name" class="form-text text-muted">type here the phone number of your
-										business</small>
-									@error('phone_number')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
+    {{-- my restaurant --}}
+    <section id="my_restaurant">
 
+     <div class="row py-2">
+      {{-- thumb image --}}
+      <div class="col-auto py-2">
+       @if (Str::startsWith($restaurant->thumb, 'https://'))
+        <img src="{{ $restaurant->thumb }}" style="width: 200px; height: 200px" class="rounded">
+       @elseif($restaurant->thumb != null)
+        <img src="{{ asset('storage/' . $restaurant->thumb) }}" alt="{{ $restaurant->name }}"
+         style="width: 200px; height: 200px" class="rounded">
+       @else
+        <img src="https://placehold.co/200x200" alt="restaurant placeholder" class="rounded">
+       @endif
+      </div>
+      {{-- name and contacts --}}
+      <div class="col-auto d-flex py-4">
+       <div class="col-auto">
+        <h2 class="text-left">{{ $restaurant->name }}</h2>
+        <div class="contacts">
+         <p><i class="fa-solid fa-location-dot"></i>{{ $restaurant->address }}</p>
+         <p><i class="fa-solid fa-envelope"></i>{{ $restaurant->contact_email }}</p>
+         <p><i class="fa-solid fa-phone"></i> {{ $restaurant->phone_number }}</p>
+         @foreach ($restaurant->types as $type)
+          <span> {{ $type->name }};</span>
+         @endforeach
+        </div>
+       </div>
+      </div>
+      {{-- edit --}}
+      <div class="col-auto p-4 text-end">
+       <a href="{{ route('admin.restaurants.edit', $restaurant) }}">
+        <i class="fa-solid fa-pen"></i>
+       </a>
+      </div>
+     </div>
 
-								<div class="my-2">
-									<label for="contact_email" class="form-label">Contact email</label>
-									<input type="text" class="form-control @error('contact_email') is-invalid @enderror" id="contact_email"
-										name="contact_email" value="{{ old('contact_email') }}" placeholder="es.example@gmail.com">
-									<small id="name" class="form-text text-muted">type here the contact email of your
-										business</small>
-									@error('contact_email')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
+   </div>
 
-								<div class="my-2">
-									<label for="logo" class="form-label">Logo</label>
-									<input type="file" class="form-control @error('logo') is-invalid @enderror" id="logo" name="logo">
-
-									<small id="name" class="form-text text-muted">add your logo image</small>
-									@error('logo')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
-
-								<div class="my-2">
-									<label for="thumb" class="form-label">Thumb</label>
-									<input type="file" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb">
-									<small id="name" class="form-text text-muted">add an image that rapresent your
-										business</small>
-									@error('thumb')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
-
-								<div class="my-2">
-									<label for="type" class="form-label">Type of restaurants</label>
-									<div class="d-flex gap-3">
-										@foreach ($typeList as $type)
-											<div class="form-check text-center ">
-												<input name="typeList[]" class="form-check-input " type="checkbox" value="{{ $type->id }}"
-													id="type-{{ $type->id }}" {{ in_array($type->id, old('typeList', [])) ? 'checked' : '' }} />
-												<label class="form-check-label" for="type-{{ $type->id }}">{{ $type->name }} </label>
-											</div>
-										@endforeach
-									</div>
-									<small id="name" class="form-text text-muted">add your restaurant's types</small>
-									@error('typeList')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-								</div>
-
-
-								<div class="my-2">
-									<label for="vat" class="form-label">Vat</label>
-									<input type="text" class="form-control @error('vat') is-invalid @enderror" id="vat" name="vat"
-										value="{{ old('vat') }}" placeholder="es.11111111111">
-									<small id="name" class="form-text text-muted">type here your VAT number</small>
-									@error('vat')
-										<div class="alert alert-danger">{{ $message }}</div>
-									@enderror
-
-								</div>
-								<button type="submit" class="btn btn-primary">Submit</button>
-							</form>
-						@else
-							<img src="{{ asset('storage/' . $restaurant->logo) }}" alt="" width="100px">
-
-							<img src="{{ asset('storage/' . $restaurant->thumb) }}" alt="" width="100px">
-							<div class="h2">{{ $restaurant->name }}</div>
-							<div class="h2">{{ $restaurant->address }}</div>
-							<div class="h2">{{ $restaurant->phone_number }}</div>
-							<div class="h2">{{ $restaurant->contact_email }}</div>
-							<div class="h2">{{ $restaurant->vat }}</div>
-							@foreach ($restaurant->types as $type)
-								<div> {{ $type->name }}</div>
-							@endforeach
-							<a href="{{ route('admin.restaurants.edit', $restaurant) }}"><button class="btn btn-primary">Edit
-									Restaurant</button></a>
-							<form action="{{ route('admin.restaurants.destroy', $restaurant) }}" method="post">
-								@csrf
-								@method('DELETE')
-								<button class="btn btn-danger" type="submit">Cancella</button>
-							</form>
-						@endif
-
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</div>
+  </div>
+ </div>
+ </section>
 @endsection
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.admin')
 
 @section('content')
-	<div class="container">
-		{{-- <h2 class="fs-4 text-secondary my-4">
+ <div class="container">
+  {{-- <h2 class="fs-4 text-secondary my-4">
 			{{ __('Dashboard') }}
 		</h2> --}}
-		<div class="row justify-content-center">
-			<div class="col">
-				{{-- <div class="card">
+  <div class="row justify-content-center">
+   <div class="col">
+    {{-- <div class="card">
 					<a href="{{ route('admin.restaurants.index') }}">il mio ristorante</a>
 				</div> --}}
-			</div>
-		</div>
+   </div>
+  </div>
 
-	</div>
+ </div>
 @endsection
