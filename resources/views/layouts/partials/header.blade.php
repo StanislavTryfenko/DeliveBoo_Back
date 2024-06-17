5c6a8ddc901b180fd7d8e0ee912c61b8ebddf625
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-17 18:43:53 +0200

    fix header

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,45 +1,45 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" id="my_header">
-	<div class="container collapse navbar-collapse" id="navbarSupportedContent">
-		<!-- Left Side Of Navbar -->
-		<ul class="navbar-nav me-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
-			</li>
-		</ul>
-		<!-- Right Side Of Navbar -->
-		<ul class="navbar-nav ml-auto">
-			<!-- Authentication Links -->
-			@guest
-				<li class="nav-item">
-					<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-				</li>
-				@if (Route::has('register'))
-					<li class="nav-item">
-						<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-					</li>
-				@endif
-			@else
-				<li class="nav-item dropdown">
-					<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-						aria-haspopup="true" aria-expanded="false" v-pre>
-						{{ Auth::user()->name }}
-					</a>
+    <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/dashboard') }}">{{ __('Home') }}</a>
+            </li>
+        </ul>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
 
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-						<a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
-						<a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-						<a class="dropdown-item" href="{{ route('logout') }}"
-							onclick="event.preventDefault();
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
+                        <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
 																											 document.getElementById('logout-form').submit();">
-							{{ __('Logout') }}
-						</a>
+                            {{ __('Logout') }}
+                        </a>
 
-						<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-							@csrf
-						</form>
-					</div>
-				</li>
-			@endguest
-		</ul>
-	</div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
+    </div>
 </nav>
