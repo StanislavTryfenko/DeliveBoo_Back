0062e156efe5a23037f3a3d28c9e30d4abb2ff98
emamoldo <moldovanemanuel92.em@gmail.com>
2024-06-17 12:01:35 +0200

    Edit the forms layouts fot the Restaurants and the Dishes

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -13,50 +13,55 @@
                 <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="mb-3">
-                        <label for="name" class="form-label">Nome</label>
+                        <label for="name" class="form-label">Name:</label>
                         <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}"
                             required>
                         @error('name')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
+                        <small id="name" class="form-text text-muted">Add the Name of your Dish</small>
                     </div>
 
                     <div class="mb-3">
-                        <label for="price" class="form-label">Prezzo</label>
+                        <label for="price" class="form-label">Price:</label>
                         <input type="text" class="form-control" id="price" name="price" value="{{ old('price') }}"
                             required>
                         @error('price')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
+                        <small id="name" class="form-text text-muted">Add the Price of your Dish</small>
                     </div>
 
                     <div class="mb-3">
-                        <label for="image" class="form-label">Immagine</label>
+                        <label for="image" class="form-label">Image:</label>
                         <input type="file" class="form-control" id="image" name="image">
                         @error('image')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
+                        <small id="name" class="form-text text-muted">Add the Image of your Dish</small>
                     </div>
 
                     <div class="mb-3">
-                        <label for="description" class="form-label">Descrizione</label>
+                        <label for="description" class="form-label">Description:</label>
                         <textarea class="form-control" id="description" name="description" rows="3">{{ old('description') }}</textarea>
                         @error('description')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
+                        <small id="name" class="form-text text-muted">Add the Description of your Dish</small>
                     </div>
 
                     <div class="mb-3">
-                        <label for="visible" class="form-label">Disponibile:</label>
+                        <label for="visible" class="form-label">Available:</label>
                         <input type="hidden" name="visible" value="0">
                         <input type="checkbox" class="form-check-input" id="visible" name="visible" value="1"
                             {{ old('visible') ? 'checked' : '' }} style="width:2rem; height: 2rem;">
                         @error('visible')
                             <div class="text-danger">{{ $message }}</div>
                         @enderror
+                        <small id="name" class="form-text text-muted">Set if this Dish is Available</small>
                     </div>
 
-                    <button type="submit" class="btn btn-primary w-100 mt-3">Crea</button>
+                    <button type="submit" class="btn btn-primary w-100 mt-3">Create</button>
                 </form>
             </div>
         </div>
--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,57 +1,56 @@
 @extends('layouts.admin')
 
 @section('content')
- <div class="container-fluid">
+<div class="container-fluid">
   <div class="row justify-content-center">
-   <div class="col-12">
+    <div class="col-12">
 
-    {{-- add your restaurant here --}}
-    <section>
-     @if ($restaurant === null)
+      {{-- add your restaurant here --}}
+      @if ($restaurant === null)
+      <section>
       @include('layouts.partials.restaurant.creation')
-     @endif
-    </section>
+      </section>
+    @else
 
-    {{-- my restaurant --}}
-    <section id="my_restaurant">
+        {{-- my restaurant --}}
+        <section id="my_restaurant">
 
-     <div class="row py-2">
-      {{-- thumb image --}}
-      <div class="col-auto py-2">
-       @if (Str::startsWith($restaurant->thumb, 'https://'))
-        <img src="{{ $restaurant->thumb }}" style="width: 200px; height: 200px" class="rounded">
-       @elseif($restaurant->thumb != null)
+        <div class="row py-2">
+          {{-- thumb image --}}
+          <div class="col-auto py-2">
+          @if($restaurant->thumb != null)
         <img src="{{ asset('storage/' . $restaurant->thumb) }}" alt="{{ $restaurant->name }}"
-         style="width: 200px; height: 200px" class="rounded">
-       @else
-        <img src="https://placehold.co/200x200" alt="restaurant placeholder" class="rounded">
-       @endif
-      </div>
-      {{-- name and contacts --}}
-      <div class="col-auto d-flex py-4">
-       <div class="col-auto">
-        <h2 class="text-left">{{ $restaurant->name }}</h2>
-        <div class="contacts">
-         <p><i class="fa-solid fa-location-dot"></i>{{ $restaurant->address }}</p>
-         <p><i class="fa-solid fa-envelope"></i>{{ $restaurant->contact_email }}</p>
-         <p><i class="fa-solid fa-phone"></i> {{ $restaurant->phone_number }}</p>
-         @foreach ($restaurant->types as $type)
-          <span> {{ $type->name }};</span>
-         @endforeach
+        style="width: 200px; height: 200px" class="rounded">
+      @else
+      <img src="https://placehold.co/200x200" alt="restaurant placeholder" class="rounded">
+    @endif
+          </div>
+          {{-- name and contacts --}}
+          <div class="col-auto d-flex py-4">
+          <div class="col-auto">
+            <h2 class="text-left">{{ $restaurant->name }}</h2>
+            <div class="contacts">
+            <p><i class="fa-solid fa-location-dot"></i>{{ $restaurant->address }}</p>
+            <p><i class="fa-solid fa-envelope"></i>{{ $restaurant->contact_email }}</p>
+            <p><i class="fa-solid fa-phone"></i> {{ $restaurant->phone_number }}</p>
+            @foreach ($restaurant->types as $type)
+        <span> {{ $type->name }};</span>
+      @endforeach
+            </div>
+          </div>
+          </div>
+          {{-- edit --}}
+          <div class="col-auto p-4 text-end">
+          <a href="{{ route('admin.restaurants.edit', $restaurant) }}">
+            <i class="fa-solid fa-pen"></i>
+          </a>
+          </div>
         </div>
-       </div>
-      </div>
-      {{-- edit --}}
-      <div class="col-auto p-4 text-end">
-       <a href="{{ route('admin.restaurants.edit', $restaurant) }}">
-        <i class="fa-solid fa-pen"></i>
-       </a>
-      </div>
-     </div>
 
-   </div>
+      </div>
 
-  </div>
- </div>
- </section>
+      </div>
+    </div>
+    </section>
+  @endif
 @endsection
