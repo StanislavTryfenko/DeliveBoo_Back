b0e95b50eafe9425646779ab88e2bc75fc8101b9
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-06-18 16:47:20 +0200

    switch restaurantcontroller to registetered user controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,32 +42,39 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name_restaurant'=> 'required|min:4|max:255',
+            //'contact_email'=> 'required|email|max:255|unique:restaurants,contact_email',
+            'address' => 'required|min:4|max:255',
+            //'phone_number' => 'required|numeric|max_digits:10|min_digits:10',
+            //'logo' => 'image|mimes:png,jpg|max:2048', 
+            'vat' => 'required|numeric|max_digits:11|min_digits:11',
+            'typeList' =>'required|exists:types,id',
         ]);
-       
+
+        //dd($request->all());
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-       
-        $test = $request->validate([
-            'name_restaurant'=> 'required|min:4|max:255',
-            'contact_email'=> 'required|email|max:255|unique:restaurants,contact_email',
-            'address' => 'required|min:4|max:255',
-            'phone_number' => 'required|numeric|max_digits:10|min_digits:10',
-            'logo' => 'image|mimes:png,jpg|max:2048', //laravel prova ad aprire il file,leggerlo e capire l'estensione tramite mimes
-            'thumb' => 'image|mimes:png,jpg|max:2048',
-            'vat' => 'required|numeric|max_digits:11|min_digits:11',
-            'typeList' =>'required|exists:types,id',
+        
+        $restaurant = Restaurant::create([
+            'user_id' => $user->id,
+            'name_restaurant' => $request->name_restaurant,
+            'slug' => Str::slug($request->name_restaurant, '-'),
+            'contact_email' => $request->contact_email,
+            'address' => $request->address,
+            'phone_number' => $request->phone_number,
+            'vat' => $request->vat,
+            'logo' => $request->has('logo') ? Storage::put('uploads', $request->logo) : null,
+            'thumb' => $request->has('thumb') ? Storage::put('uploads', $request->thumb) : null,
         ]);
         
-        dd($test);
-        //Logica creazione ristorante 
-       
-
+        if ($request->has('typeList')) {
+            $restaurant->types()->attach($request['typeList']);
+        }
         
-
-
+        //dd($user, $restaurant);
 
         event(new Registered($user));
 
