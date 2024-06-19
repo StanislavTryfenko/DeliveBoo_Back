f5454ad1636ceb8b72aff2c698ad91623b173843
antden99 <antoniodenigris@icloud.com>
2024-06-19 16:28:35 +0200

    added api routes for filtering types

--- app/Http/Controllers/API/RestaurantController.php
+++ app/Http/Controllers/API/RestaurantController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Restaurant;
 use App\Models\Type;
+use Illuminate\Support\Facades\DB;
 
 class RestaurantController extends Controller
 {
@@ -42,6 +43,30 @@ class RestaurantController extends Controller
 
     public function filteredType(Request $request)
     {
-      //
+        $lista = [2, 4, 5];
+        $countLista = count($lista);
+        /*
+SELECT `name_restaurant`,`restaurants`.`id` FROM `restaurants`
+JOIN `restaurant_type` ON `restaurants`.`id` = `restaurant_type`.`restaurant_id`
+JOIN `types` ON `restaurant_type`.`type_id` = `types`.`id`
+WHERE `types`.`id` IN (2,4,5)
+GROUP BY `restaurants`.`id`
+HAVING COUNT(`types`.`id`) = 3;
+*/
+
+        $query = DB::table('restaurants')
+            ->select('restaurants.name_restaurant', 'restaurants.id')
+            ->join('restaurant_type', 'restaurants.id', '=', 'restaurant_type.restaurant_id')
+            ->join('types', 'restaurant_type.type_id', '=', 'types.id')
+            ->whereIn('types.id', $lista)
+            ->groupBy('restaurants.id')
+            ->havingRaw('COUNT(types.id) = ?', [$countLista])
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'response' => $query,
+            'lunghezza' => $countLista,
+        ]);
     }
 }
