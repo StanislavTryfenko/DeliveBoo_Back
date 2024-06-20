5faa4a22e61b4a4d2d4dba05f8e5ab98e7793935
emamoldo <moldovanemanuel92.em@gmail.com>
2024-06-20 10:36:32 +0200

    add the logo to the navbar

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -2,8 +2,10 @@
     <div class="container collapse navbar-collapse" id="navbarSupportedContent">
         <!-- Left Side Of Navbar -->
         <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/dashboard') }}">{{ __('Home') }}</a>
+            <li class="nav-item fs-1">
+                <a class="nav-link" href="{{ url('/dashboard') }}">
+                    <strong class="text-info-emphasis">Saladas</strong>
+                </a>
             </li>
         </ul>
         <!-- Right Side Of Navbar -->
@@ -20,8 +22,8 @@
                 @endif
             @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
 
@@ -30,7 +32,7 @@
                         {{-- <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a> --}}
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
-							 document.getElementById('logout-form').submit();">
+                                                                                                                                                 document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
 
