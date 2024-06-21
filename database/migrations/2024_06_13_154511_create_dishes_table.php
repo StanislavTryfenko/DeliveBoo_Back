faa8fc957cfa9f3d7741567341f3d54928b82817
emamoldo <moldovanemanuel92.em@gmail.com>
2024-06-21 15:30:55 +0200

    Add the Dati del Ristorante to the register form

--- database/migrations/2024_06_13_154511_create_dishes_table.php
+++ database/migrations/2024_06_13_154511_create_dishes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
-            $table->decimal('price',5,2);
+            $table->decimal('price', 5, 2);
             $table->string('image')->nullable();
             $table->text('description')->nullable();
             $table->boolean('visible')->default(0);
