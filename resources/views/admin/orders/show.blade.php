7d3baed8bdf7ca9b3cadbaa74ef432e3481f5135
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-26 17:30:39 +0200

    add various layout fixes

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container-fluid p-4">
-    <div class="row">
-        <div class="col-6">
-            <h2>Ordine # {{ $order->id }}</h2>
-            <p class="mt-4"><strong>Prezzo totale: </strong>€ {{ $order->total_price }}</p>
-            <p><strong>Data dell'ordine: </strong>{{ $order->date }}</p>
-            <p><strong>Stato dell'ordine: </strong>{{ $order->status }}</p>
-        </div>
-        <div class="col-6">
-            <h3>Dati cliente</h3>
-            <p><strong>Nome: </strong>{{ $order->customer_name }}</p>
-            <p><strong>Cognome: </strong>{{ $order->customer_lastname }}</p>
-            <p><strong>Email: </strong>{{ $order->customer_email }}</p>
-            <p><strong>Numero di telefono: </strong>{{ $order->customer_phone_number }}</p>
-            <p><strong>Indirizzo: </strong>{{ $order->customer_address }}</p>
-        </div>
-        <div class="col-12">
-            <h3>Piatti</h3>
-            @foreach ($order->dishes as $dish)
-                <div class="card my-3">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $dish->name }}</h4>
-                        <p class="card-text">Quantità: {{ $dish->pivot->dish_quantity }}</p>
-                        <p class="card-text">Prezzo: € {{ $dish->price }}</p>
+    <div class="container-fluid p-4">
+        <div class="row">
+            <div class="col-6">
+                <h2>Ordine # {{ $order->id }}</h2>
+                <p class="mt-4"><strong>Prezzo totale: </strong>€ {{ $order->total_price }}</p>
+                <p><strong>Data dell'ordine: </strong>{{ $order->date }}</p>
+                <p><strong>Stato dell'ordine: </strong>{{ $order->status }}</p>
+            </div>
+            <div class="col-6">
+                <h3>Dati cliente</h3>
+                <p><strong>Nome: </strong>{{ $order->customer_name }}</p>
+                <p><strong>Cognome: </strong>{{ $order->customer_lastname }}</p>
+                <p><strong>Email: </strong>{{ $order->customer_email }}</p>
+                <p><strong>Numero di telefono: </strong>{{ $order->customer_phone_number }}</p>
+                <p><strong>Indirizzo: </strong>{{ $order->customer_address }}</p>
+            </div>
+            <div class="col-12">
+                <h3>Piatti</h3>
+                @foreach ($order->dishes as $dish)
+                    <div class="card my-3">
+                        <div class="card-body">
+                            <h4 class="card-title mb-3">{{ $dish->name }}</h4>
+                            <p class="card-text">Quantità: {{ $dish->pivot->dish_quantity }}</p>
+                            <p class="card-text">Prezzo: € {{ $dish->price }}</p>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
-</div>
 @endsection
