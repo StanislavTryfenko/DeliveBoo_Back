8e3b72c2fa288d94d680a6440e881bd8d2417421
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-06-27 20:53:04 +0200

    fix mail markdown

--- resources/views/mail/new-lead-message.blade.php
+++ resources/views/mail/new-lead-message.blade.php
@@ -1,24 +1,23 @@
 <x-mail::message>
-    @if ($isUser)
-        Grazie per aver effettuato un ordine su {{ config('app.name') }},{{ $istance['customer_name'] }}<br>
-        Ti confermiamo di aver ricevuto l'ordine nr.{{ $istance['id'] }}<br>
-        contenente i segunti prodotti :<br>
-        @foreach ($order as $dish)
-            - {{ $dish['name'] }} x {{ $dish['quantity'] }}<br>
-        @endforeach
-    @else
-        Gentile {{ $istance['name_restaurant'] }}<br>
-        Hai appena ricevuto un nuovo ordine.<br>
-        Clicca il bottone per accedere alla tua area amministrativa!<br>
-    @endif
+@if ($isUser)
+Grazie per aver effettuato un ordine su {{ config('app.name') }}, {{ $istance['customer_name'] }}<br>
+Ti confermiamo di aver ricevuto l'ordine nr.{{ $istance['id'] }}<br>
+contenente i seguenti prodotti:<br>
+@foreach ($order as $dish)
+- {{ $dish['name'] }} x {{ $dish['quantity'] }}<br>
+@endforeach
+@else
+Gentile {{ $istance['name_restaurant'] }}<br>
+Hai appena ricevuto un nuovo ordine.<br>
+Clicca il bottone per accedere alla tua area amministrativa!<br>
+@endif
 
-
-    @if ($isUser)
-        Grazie,{{ config('app.name') }}
-    @else
-        <x-mail::button :url="'http://127.0.0.1:8000/login'">
-            Area Amministrativa
-        </x-mail::button>
-    @endif
-
+@if ($isUser)
+Grazie, {{ config('app.name') }}
+@else
+<x-mail::button :url="'http://127.0.0.1:8000/login'">
+Area Amministrativa
+</x-mail::button>
+<br>
+@endif
 </x-mail::message>
